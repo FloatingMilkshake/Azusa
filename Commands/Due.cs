9880060da94f3e7d9e3af02f072fb2368150eca8
FloatingMilkshake <me@floatingmilkshake.com>
2025-02-21 19:19:08 -0500

    Due: Add [RemainingText] to filter parameter

--- Commands/Due.cs
+++ Commands/Due.cs
@@ -4,7 +4,7 @@ public static class Due
 {
     [Command("due"), Description("Get due assignments from Canvas.")]
     [AllowedProcessors(typeof(TextCommandProcessor))]
-    public static async Task DueCommand(TextCommandContext ctx, [Parameter("filter"), Description("The time to filter by.")] string filter)
+    public static async Task DueCommand(TextCommandContext ctx, [Parameter("filter"), Description("The time to filter by."), RemainingText] string filter)
     {
         await ctx.RespondAsync("Working on it...");
         
