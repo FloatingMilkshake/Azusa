95066f0c0045f75a25a2b86103a510ab77b3fab7
FloatingMilkshake <me@floatingmilkshake.com>
2025-02-21 19:40:22 -0500

    Add shell command

---
+++ Commands/Shell.cs
@@ -0,0 +1,93 @@
+namespace Azusa.Commands;
+
+public class Shell
+{
+    // The idea for this command, and a lot of the code, is taken from Erisa's Lykos. References are linked below.
+    // https://github.com/Erisa/Lykos/blob/5f9c17c/src/Modules/Owner.cs#L116-L144
+    // https://github.com/Erisa/Lykos/blob/822e9c5/src/Modules/Helpers.cs#L36-L82
+    [Command("shell")]
+    [TextAlias("sh")]
+    [Description("Run a shell command.")]
+    [RequireApplicationOwner]
+    public static async Task ShellCommand(TextCommandContext ctx,
+        [Parameter("command"), Description("The command to run, including any arguments.")] string command)
+    {
+        var cmdResponse = await ShellCommand(command);
+        string response;
+        
+        response = cmdResponse.Output.Length > 1900
+            ? $"Finished with exit code `{cmdResponse.ExitCode}`, but the output was too long to post here."
+            : $"Finished with exit code `{cmdResponse.ExitCode}`! Output: ```\n{cmdResponse.Output}```";
+        
+        await ctx.FollowupAsync(new DiscordFollowupMessageBuilder().WithContent(response));
+    }
+
+    private static async Task<ShellCommandResponse> ShellCommand(string command)
+    {
+        var osDescription = RuntimeInformation.OSDescription;
+        string fileName;
+        string args;
+        var escapedArgs = command.Replace("\"", "\\\"");
+
+        if (osDescription.Contains("Windows"))
+        {
+            fileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+            args = $"-Command \"{escapedArgs} 2>&1\"";
+        }
+        else
+        {
+            // Assume Linux if OS is not Windows because I'm too lazy to bother with specific checks right now, might implement that later
+            fileName = Environment.GetEnvironmentVariable("SHELL");
+            if (!File.Exists(fileName)) fileName = "/bin/sh";
+
+            args = $"-c \"{escapedArgs}\"";
+        }
+
+        Process proc = new()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = args,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true
+            }
+        };
+
+        proc.Start();
+        var result = await proc.StandardOutput.ReadToEndAsync();
+        await proc.WaitForExitAsync();
+
+        return new ShellCommandResponse(proc.ExitCode, result);
+    }
+}
+
+internal class ShellCommandResponse
+{
+    public ShellCommandResponse(int exitCode, string output, string error)
+    {
+        ExitCode = exitCode;
+        Output = output;
+        Error = error;
+    }
+    
+    public ShellCommandResponse(int exitCode, string output)
+    {
+        ExitCode = exitCode;
+        Output = output;
+        Error = default;
+    }
+
+    public ShellCommandResponse()
+    {
+        ExitCode = default;
+        Output = default;
+        Error = default;
+    }
+
+    public int ExitCode { get; set; }
+    public string Output { get; set; }
+    public string Error { get; set; }
+}
