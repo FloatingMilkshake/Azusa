5a0253c27224dbbdaf47530e4e71c6a5e5b5b3b9
FloatingMilkshake <me@floatingmilkshake.com>
2025-12-15 19:30:02 -0500

    Upgrade DSharpPlus to 5.0.0-nightly-02563

--- Events/MessageEvents.cs
+++ Events/MessageEvents.cs
@@ -6,41 +6,48 @@ public static class MessageEvents
 {
     public static async Task MessageCreated(DiscordClient client, MessageCreatedEventArgs e)
     {
-        #region MonitoRSS monitoring
-        
+        await DoMonitoRSSCheckAsync(client, e);
+
+        await ParseWindowsInsidersRssAsync(client, e);
+
+        await DoTodoCountAsync(client, e);
+    }
+
+    // ReSharper disable once InconsistentNaming
+    private static async Task DoMonitoRSSCheckAsync(DiscordClient _, MessageCreatedEventArgs e)
+    {
         if (e.Channel.Id == 1408962751153569944 && e.Author.Id == 944784076735414342)
         {
             // Log time of latest message from MonitoRSS in feed channel
             await Program.Redis.StringSetAsync("monitorssLastDelivery", $"{JsonConvert.SerializeObject(DateTime.UtcNow)}@{e.Message.JumpLink}");
         }
-        
-        #endregion MonitoRSS monitoring
-        
-        #region insider RSS feed parsing for /announcebuild
-        
+    }
+
+    private static async Task ParseWindowsInsidersRssAsync(DiscordClient client, MessageCreatedEventArgs e)
+    {
         // ignore self
         if (e.Message.Author?.Id == client.CurrentUser.Id)
             return;
-        
+
         // try to match content with Insider URL pattern
         var insiderUrlPattern = RegularExpressions.InsiderUrlPattern();
-        
+
         // ignore non-matching messages or messages that are not from RSS articles
         if (!insiderUrlPattern.IsMatch(e.Message.Content) || !e.Message.Content.Contains("📰"))
             return;
-        
+
         var insiderUrlMatch = insiderUrlPattern.Match(e.Message.Content);
         var buildNumber1 = insiderUrlMatch.Groups[1].Value;
         var buildNumber2 = insiderUrlMatch.Groups[2].Value;
         var channel1 = insiderUrlMatch.Groups[3].Value;
         var channel2 = insiderUrlMatch.Groups[4].Value;
-        
+
         // format channel names correctly
         // canary -> Canary Channel
         // dev -> Dev Channel
         // beta -> Beta Channel
         // release-preview -> Release Preview Channel
-        
+
         channel1 = channel1 switch
         {
             "canary" => "Canary Channel",
@@ -49,7 +56,7 @@ public static class MessageEvents
             "release-preview" => "Release Preview Channel",
             _ => string.Empty
         };
-        
+
         channel2 = channel2 switch
         {
             "canary" => "Canary Channel",
@@ -58,7 +65,7 @@ public static class MessageEvents
             "release-preview" => "Release Preview Channel",
             _ => string.Empty
         };
-        
+
         // assemble /announcebuild command
         // format is: /announcebuild windows_version:WINDOWS_VERSION build_number:BUILD_NUMBER blog_link:BLOG_LINK insider_role1:FIRST_ROLE insider_role2:SECOND_ROLE
         // insider_role2 is optional
@@ -71,25 +78,50 @@ public static class MessageEvents
                 ? buildNumber1
                 : buildNumber2;
         buildNumber = buildNumber.Replace('-', '.');
-        
+
         var blogLink = insiderUrlMatch.ToString();
-        
+
         var command = $"/announcebuild build_number:{buildNumber} blog_link:{blogLink} insider_role1:{channel1}";
         if (channel2 != string.Empty) command += $" insider_role2:{channel2}";
-        
+
         // send command to channel
         // ReSharper disable once PossibleNullReferenceException; channel cannot be null
         var msg = await e.Message.Channel.SendMessageAsync(command);
-        
+
         // suppress embed
         await Task.Delay(1000);
         await msg.ModifyEmbedSuppressionAsync(true);
-        
-        if (e.Message.Embeds.Any(x => 
+
+        if (e.Message.Embeds.Any(x =>
                 (x.Description?.Contains("please note", StringComparison.OrdinalIgnoreCase) ?? false) ||
                 (x.Description?.Contains("note:", StringComparison.OrdinalIgnoreCase) ?? false)))
             await msg.CreateReactionAsync(DiscordEmoji.FromName(client, ":bangbang:"));
-        
-        #endregion insider RSS feed parsing for /announcebuild
+    }
+
+    private static async Task DoTodoCountAsync(DiscordClient _, MessageCreatedEventArgs e)
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        if (e.Message.Channel.Guild is not null
+            && e.Message.Channel.Guild.Id == 799644062973427743
+            && e.Message.Channel.Name.StartsWith("todo")
+            && e.Message.Channel.Name.Length <= 7)
+        {
+            var numTodoItems = 0;
+            var messages = await e.Message.Channel.GetMessagesAsync().ToListAsync();
+            foreach (var message in messages)
+            {
+                if (message.Author.Id == 455432936339144705 &&
+                    (message.Content.StartsWith("[ ]") || message.Content.StartsWith("- ")))
+                {
+                    numTodoItems++;
+                }
+            }
+
+            await e.Message.Channel.ModifyAsync(x =>
+            {
+                x.Name = $"todo-{numTodoItems}";
+                x.AuditLogReason = "Updating number of todo items.";
+            });
+        }
     }
 }
