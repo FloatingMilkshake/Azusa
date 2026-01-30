126a0b167a671a6409f530e7292a927e2295c358
FloatingMilkshake <me@floatingmilkshake.com>
2026-01-30 11:08:21 -0500

    Add err

---
+++ Commands/Err.cs
@@ -0,0 +1,18 @@
+﻿namespace Azusa.Commands;
+
+public static class Err
+{
+    [Command("err")]
+    [Description("Look up an error code with the Microsoft Error Lookup Tool.")]
+    [AllowedProcessors(typeof(TextCommandProcessor))]
+    public static async Task ErrCommand(TextCommandContext ctx,
+        [Parameter("code"), Description("The error code to look up.")] string code)
+    {
+        var result = (await (await Program.HttpClient.GetAsync($"https://err.milkshake.services?code={code}")).Content.ReadAsStringAsync())
+            .Replace("<pre>", "").Replace("</pre>", "");
+
+        result = $"```\n{result}\n```";
+
+        await Helpers.StringHelpers.SplitStringAsync(result, true, ctx: ctx);
+    }
+}
