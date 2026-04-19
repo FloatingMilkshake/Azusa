2e69fe3e56283655276ccf3702eb877f5635d0c9
FloatingMilkshake <me@floatingmilkshake.com>
2026-04-18 21:24:06 -0400

    Remove Windows Insiders RSS feed parsing

--- Events/MessageEvents.cs
+++ Events/MessageEvents.cs
@@ -4,83 +4,10 @@ internal static class MessageEvents
 {
     internal static async Task HandleMessageCreatedEventAsync(DiscordClient client, MessageCreatedEventArgs e)
     {
-        await ParseWindowsInsidersRssAsync(client, e);
-
-        await CheckRssFeedArticlesForUpdatesAsync(client, e);
+        await CheckRssFeedArticlesForUpdatesAsync(e);
     }
 
-    private static async Task ParseWindowsInsidersRssAsync(DiscordClient client, MessageCreatedEventArgs e)
-    {
-        // ignore self
-        if (e.Message.Author?.Id == client.CurrentUser.Id)
-            return;
-
-        // ignore non-matching messages or messages that are not from MonitoRSS
-        if (!Setup.Constants.RegularExpressions.WindowsInsiderBlogUrlPattern.IsMatch(e.Message.Content) || e.Message.Author.Id != 944784076735414342)
-            return;
-
-        var insiderUrlMatch = Setup.Constants.RegularExpressions.WindowsInsiderBlogUrlPattern.Match(e.Message.Content);
-        var buildNumber1 = insiderUrlMatch.Groups[1].Value;
-        var buildNumber2 = insiderUrlMatch.Groups[2].Value;
-        var channel1 = insiderUrlMatch.Groups[3].Value;
-        var channel2 = insiderUrlMatch.Groups[4].Value;
-
-        // format channel names correctly
-        // canary -> Canary Channel
-        // dev -> Dev Channel
-        // beta -> Beta Channel
-        // release-preview -> Release Preview Channel
-
-        channel1 = channel1 switch
-        {
-            "canary" => "Canary Channel",
-            "dev" => "Dev Channel",
-            "beta" => "Beta Channel",
-            "release-preview" => "Release Preview Channel",
-            _ => string.Empty
-        };
-
-        channel2 = channel2 switch
-        {
-            "canary" => "Canary Channel",
-            "dev" => "Dev Channel",
-            "beta" => "Beta Channel",
-            "release-preview" => "Release Preview Channel",
-            _ => string.Empty
-        };
-
-        // assemble /announcebuild command
-        // format is: /announcebuild windows_version:WINDOWS_VERSION build_number:BUILD_NUMBER blog_link:BLOG_LINK insider_role1:FIRST_ROLE insider_role2:SECOND_ROLE
-        // insider_role2 is optional
-        // if two build numbers are present, pick the higher one
-        // if a build number contains a hyphen, replace it with a dot (ex. 22635-3430 -> 22635.3430)
-
-        var buildNumber = buildNumber2 == string.Empty
-            ? buildNumber1
-            : string.Compare(buildNumber1, buildNumber2, StringComparison.Ordinal) > 0
-                ? buildNumber1
-                : buildNumber2;
-        buildNumber = buildNumber.Replace('-', '.');
-
-        var blogLink = insiderUrlMatch.ToString();
-
-        var command = $"/announcebuild build_number:{buildNumber} blog_link:{blogLink} insider_role1:{channel1}";
-        if (channel2 != string.Empty) command += $" insider_role2:{channel2}";
-
-        // send command to channel
-        var msg = await e.Message.Channel.SendMessageAsync(command);
-
-        // suppress embed
-        await Task.Delay(1000);
-        await msg.ModifyEmbedSuppressionAsync(true);
-
-        if (e.Message.Embeds.Any(x =>
-                (x.Description?.Contains("please note", StringComparison.OrdinalIgnoreCase) ?? false) ||
-                (x.Description?.Contains("note:", StringComparison.OrdinalIgnoreCase) ?? false)))
-            await msg.CreateReactionAsync(DiscordEmoji.FromName(client, ":bangbang:"));
-    }
-
-    private static async Task CheckRssFeedArticlesForUpdatesAsync(DiscordClient _, MessageCreatedEventArgs e)
+    private static async Task CheckRssFeedArticlesForUpdatesAsync(MessageCreatedEventArgs e)
     {
         List<string> matches = [
             "node_exporter",
--- Setup/Constants.cs
+++ Setup/Constants.cs
@@ -4,12 +4,10 @@ internal static class Constants
 {
     internal static readonly HttpClient HttpClient = new();
     internal static readonly List<ulong> PanicAuthorizedUsers = [455432936339144705, 208935109485789184, 455428041586376729, 573984492713279512];
+
     internal class RegularExpressions
     {
-#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
-        internal static readonly Regex WindowsInsiderBlogUrlPattern = new(@"https:\/\/blogs\.windows\.com\/windows-insider\/.+windows-11.+build[s]?-(?:(\d+(?:-\d+)?)(?:-and-(\d+-\d+)?){0,1})(?:.+?(?:(canary|dev|beta|release-preview)(?:(?:-and)?-(canary|dev|beta|release-preview))*)?-channel[s]?.*)?\/");
         internal static readonly Regex CdnFileNamePattern = new(@"[^/\\&\?#]+\.\w*(?=([\?&#].*$|$))");
         internal static readonly Regex CdnFileExtensionPattern = new(@"\.\w*(?=([\?&#].*$|$))");
-#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
     }
 }
