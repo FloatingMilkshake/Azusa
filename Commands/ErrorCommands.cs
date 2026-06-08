c3b03ffbf56d6adadd9b97cdd39bc123f00ae1c1
FloatingMilkshake <me@floatingmilkshake.com>
2026-06-07 23:36:47 -0400

    err: Restrict permissions, add some error handling

--- Commands/ErrorCommands.cs
+++ Commands/ErrorCommands.cs
@@ -5,21 +5,30 @@ internal static class ErrorCommands
     [Command("error")]
     [TextAlias("err")]
     [Description("Look up an error code with the Microsoft Error Lookup Tool.")]
+    [RequireCatRole]
     [AllowedProcessors(typeof(TextCommandProcessor))]
     public static async Task ErrorCommandAsync(TextCommandContext ctx,
         [Parameter("code"), Description("The error code to look up.")] string code)
     {
-        var result = (await (await Setup.Constants.HttpClient.GetAsync($"https://err.milkshake.services?code={code}")).Content.ReadAsStringAsync())
+        var response = await Setup.Constants.HttpClient.GetAsync($"https://err.milkshake.services?code={code}");
+        if (response.IsSuccessStatusCode)
+        {
+            var result = (await response.Content.ReadAsStringAsync())
             .Replace("<pre>", "").Replace("</pre>", "");
 
-        result = $"```\n{result}\n```";
+            result = $"```\n{result}\n```";
 
-        var splitResponse = result.SplitForDiscord();
+            var splitResponse = result.SplitForDiscord();
 
-        await ctx.RespondAsync(splitResponse.First());
-        foreach (var part in splitResponse.Skip(1))
+            await ctx.RespondAsync(splitResponse.First());
+            foreach (var part in splitResponse.Skip(1))
+            {
+                await ctx.Channel.SendMessageAsync(part);
+            }
+        }
+        else
         {
-            await ctx.Channel.SendMessageAsync(part);
+            await ctx.RespondAsync($"{(int)response.StatusCode} {response.ReasonPhrase}");
         }
     }
 }
