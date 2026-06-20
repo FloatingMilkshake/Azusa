9b777639a06038a2d4ee4bc163a1d32e4d955b71
FloatingMilkshake <me@floatingmilkshake.com>
2026-06-19 23:56:36 -0400

    Add "Is this published?" msg context menu command

---
+++ Commands/AnnouncementCommands.cs
@@ -0,0 +1,18 @@
+﻿namespace Azusa.Commands;
+
+internal class AnnouncementCommands
+{
+    [Command("Is this published?")]
+    [AllowedProcessors(typeof(MessageCommandProcessor))]
+    [SlashCommandTypes(DiscordApplicationCommandType.MessageContextMenu)]
+    [InteractionInstallType(DiscordApplicationIntegrationType.GuildInstall, DiscordApplicationIntegrationType.UserInstall)]
+    [InteractionAllowedContexts(DiscordInteractionContextType.Guild)]
+    public static async Task AnnouncementIsThisPublishedMessageContextMenuCommandAsync(MessageCommandContext ctx, DiscordMessage targetMessage)
+    {
+        var response = targetMessage.Flags?.HasFlag(DiscordMessageFlags.Crossposted) ?? false
+            ? "This message is **published**!"
+            : "This message is **not published**!";
+
+        await ctx.RespondAsync(response, ephemeral: true);
+    }
+}
