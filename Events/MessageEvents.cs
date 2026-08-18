f532c7b3df6beb15f1fbd0afd1746c72f7cd11a4
FloatingMilkshake <me@floatingmilkshake.com>
2026-08-18 00:14:59 -0400

    Restrict RSS forwarding to single channel, forward webhook messages

--- Events/MessageEvents.cs
+++ Events/MessageEvents.cs
@@ -19,19 +19,14 @@ internal static class MessageEvents
             "technitium",
             "caddy",
             "anubis",
-            "sonarr",
-            "radarr",
-            "lidarr",
-            "prowlarr",
             "immich",
-            "koi",
-	    "wivrn"
+	        "wivrn"
         ];
 
-        // MonitoRSS only
-        if (e.Message.Author.Id != 944784076735414342) return;
-
-        if (matches.Any(m => e.Message.Content.Contains(m, StringComparison.OrdinalIgnoreCase)))
+        if ((e.Message.Author.Id == 944784076735414342 || e.Message.WebhookMessage.Value)
+            && e.Channel.Id == 1408962751153569944
+            && (matches.Any(m => e.Message.Content.Contains(m, StringComparison.OrdinalIgnoreCase))
+            || matches.Any(m => e.Message.Embeds.Any(e => e.Title.Contains(m, StringComparison.OrdinalIgnoreCase)))))
         {
             await Task.Delay(5000); // give the message a bit to load embeds
             await e.Message.ForwardAsync(1409187775005331566);
